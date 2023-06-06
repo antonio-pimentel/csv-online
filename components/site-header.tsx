7be1f257799b7479cca64e161144b0a253a50e89
António Pimentel <antonio.mesquita.pimentel@gmail.com>
2023-06-06 22:03:04 +0000

    Remove nav from header.

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -3,14 +3,17 @@ import Link from "next/link"
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
 import { Icons } from "@/components/icons"
-import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
 
 export function SiteHeader() {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-        <MainNav items={siteConfig.mainNav} />
+        <div className="flex gap-6 md:gap-10">
+          <Link href="/" className="flex items-center space-x-2">
+            <span className="inline-block font-bold">CSV Editor</span>
+          </Link>
+        </div>
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
             <Link
