edbf146f42cfee5f3f4c1641d8009fed290d1ca4
António Pimentel <antonio.mesquita.pimentel@gmail.com>
2023-06-07 13:16:02 +0000

    Add export csv

--- components/main.tsx
+++ components/main.tsx
@@ -2,10 +2,18 @@
 
 import { useState } from "react"
 import { ColumnDef } from "@tanstack/react-table"
-import { ArrowUpDown, Edit2, X } from "lucide-react"
+import { ArrowUpDown, Edit2, MoreVertical, X } from "lucide-react"
 import Papa from "papaparse"
 
 import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 import { Input } from "@/components/ui/input"
 import { DataTable } from "@/components/data-table"
 
@@ -16,6 +24,22 @@ export function MainComponent() {
   const actionColumns: ColumnDef<any>[] = [
     {
       id: "actions",
+      header: () => (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreVertical className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {/* <DropdownMenuLabel>Actions</DropdownMenuLabel> */}
+            <DropdownMenuItem onClick={exportCSV}>Export csv</DropdownMenuItem>
+            {/* <DropdownMenuSeparator /> */}
+            {/* <DropdownMenuItem>View customer</DropdownMenuItem> */}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      ),
       cell: ({ row }) => {
         return (
           <div className="flex gap-2">
@@ -78,16 +102,31 @@ export function MainComponent() {
       })
   }
 
+  const exportCSV = () => {
+    const csv = Papa.unparse(data, { delimiter: ";", header: true })
+    const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" })
+    const link = document.createElement("a")
+    const url = URL.createObjectURL(blob)
+    link.setAttribute("href", url)
+    link.setAttribute("download", "data.csv")
+    link.style.visibility = "hidden"
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+  }
+
   return (
     <div>
-      <div className="grid w-full max-w-sm items-center gap-1.5">
-        <Input
-          id="csvfile"
-          type="file"
-          className="cursor-pointer"
-          accept=".csv"
-          onChange={changeHandler}
-        />
+      <div className="flex gap-4">
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Input
+            id="csvfile"
+            type="file"
+            className="cursor-pointer"
+            accept=".csv"
+            onChange={changeHandler}
+          />
+        </div>
       </div>
       {data.length > 0 && (
         <div className="mt-8">
