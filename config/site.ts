4c319d3f6712947ebe8c84c32ebd4f83826ab25c
António Pimentel <antonio.mesquita.pimentel@gmail.com>
2023-06-09 01:00:42 +0000

    Update README

--- config/site.ts
+++ config/site.ts
@@ -3,7 +3,7 @@ export type SiteConfig = typeof siteConfig
 export const siteConfig = {
   name: "CSV Viewer",
   description:
-    "Import, view, edit and export csv files online, from your browser.",
+    "Import, view, edit and export csv files online, from the browser.",
   mainNav: [],
   links: {
     github: "https://github.com/antonio-pimentel/csv-online",
